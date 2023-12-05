7176a207b6e453824c2d58d5776d69c02fbce50d
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-05 20:31:03 +0600

    Add navigation route

--- app/src/main/java/com/yhr/jfj/navigation/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/navigation/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.yhr.jfj.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +25,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home"
+                    ){
+                        composable("home"){
+                            HomeScreen(navController)
+                        }
+
+                        composable("second"){
+                            SecondScreen(navController)
+                        }
+
+                        composable("last"){
+                            LastScreen(navController)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigationTheme {
-        Greeting("Android")
     }
 }
