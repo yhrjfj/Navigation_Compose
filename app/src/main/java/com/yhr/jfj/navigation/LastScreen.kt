8f693f0b1873d21e39f365beb4083819687d8114
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-12-05 20:14:49 +0600

    Add three screens

---
+++ app/src/main/java/com/yhr/jfj/navigation/LastScreen.kt
@@ -0,0 +1,42 @@
+package com.yhr.jfj.navigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LastScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Go To Home Screen")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Go To Previous Screen")
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LastScreenPreview(){
+    LastScreen()
+}
